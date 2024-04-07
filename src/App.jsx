7350316f3bf67b7b404aee258c0ebaad95d56d0a
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2024-04-07 15:04:32 +0600

    Chore:for the get and set to the editor data on firebase

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import snarkdown from 'snarkdown';
+import {initializeApp} from 'firebase/app';
+import {getFirestore,collection,onSnapshot, doc,setDoc} from "firebase/firestore"
+
+import { config } from './firebase';
+
+const firebaseApp = initializeApp(config.firebase);
+const firestore = getFirestore(firebaseApp);
+
+// give us a reference to the 'markdowns' collection
+
+const markdowns = collection(firestore,'markdowns')
+
+const docRef = doc(firestore,'markdowns','1')
+
+
+
+
+const App = () => {
+  const [text, setText] = useState('');
+
+  console.log(text)
+
+  // Function to safely set inner HTML
+  const createMarkup = (markdown) => {
+    return { __html: snarkdown(markdown) };
+  };
+
+  useEffect(()=>{
+  
+     
+    onSnapshot(docRef,(doc)=>{
+      if(doc.exists()){
+        setText(doc.data().markdown)
+      }
+    })
+
+  },[])
+
+  function handleChange(e){
+    setText(e.target.value)
+    setDoc(docRef,{markdown:e.target.value,htmlText:snarkdown(e.target.value)})
+    
+
+
+  }
+
+  return (
+    <div id="editor">
+      <textarea 
+        name="" 
+        id="in" 
+        cols="30" 
+        rows="10" 
+        placeholder='markdown'
+        onChange={handleChange} 
+        value={text}
+      ></textarea>
+
+      <div id="out" dangerouslySetInnerHTML={createMarkup(text)}
+
+       
+       
+
+
+      />
+      
+      <pre id="code">
+
+        <p>{!text ? "Your code goes here...":snarkdown(text)}</p>
+      </pre>
+
+
+    </div>
+  );
+};
+
+export default App;
